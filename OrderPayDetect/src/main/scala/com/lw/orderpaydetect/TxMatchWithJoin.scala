7a596ae7ea1f96481ec7fa674c539347faaee224
Virgil <wyluowen@163.com>
2021-07-23 14:27:12 +0800

    双流互相等待，没匹配到两流数据都会丢失

--- OrderPayDetect/src/main/scala/com/lw/orderpaydetect/TxMatchWithJoin.scala
+++ OrderPayDetect/src/main/scala/com/lw/orderpaydetect/TxMatchWithJoin.scala
@@ -31,7 +31,7 @@ object TxMatchWithJoin {
     // 2. 读取到账事件数据
     val resource2 = getClass.getResource("/ReceiptLog.csv")
     val receiptEventStream = env.readTextFile(resource2.getPath)
-      //    val orderEventStream = env.socketTextStream("localhost", 7777)
+//        val receiptEventStream = env.socketTextStream("localhost", 7778)
       .map( data => {
         val arr = data.split(",")
         ReceiptEvent(arr(0), arr(1), arr(2).toLong)
@@ -41,8 +41,8 @@ object TxMatchWithJoin {
 
     val resultStream = orderEventStream.intervalJoin(receiptEventStream)
       // orderEventStream事件最多等receiptEventStream事件5秒，
-      // receiptEventStream事件最多等orderEventStream事件3秒
-      .between(Time.seconds(-3), Time.seconds(5))
+      // receiptEventStream事件最多等orderEventStream事件1秒
+      .between(Time.seconds(-1), Time.seconds(5))
       .process( new TxMatchWithJoinResult() )
 
     resultStream.print()
